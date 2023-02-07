69c4d30cedd3cf193e35f8fb1d33d096a33254df
muzardemoses <muzardemoses@gmail.com>
2023-02-07 18:03:08 +0100

    replaced browser router with hash-router

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter} from 'react-router-dom';
+import { HashRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter basename={process.env.PUBLIC_URL}>
    <React.StrictMode>
     <App />
    </React.StrictMode>
- </BrowserRouter>
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
